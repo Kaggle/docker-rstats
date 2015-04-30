646a6815c08d0e0556d2883cc38a889b1c508815
Ben Hamner <ben@benhamner.com>
2015-04-30 15:06:14 -0700

    attempted h2o fix

--- package_installs.R
+++ package_installs.R
@@ -2,3 +2,7 @@ library(devtools)
 install_github("hadley/readr")
 install_github('dmlc/xgboost',subdir='R-package')
 install_github("jkrijthe/Rtsne")
+
+# try loading the h2o package
+library(h2o)
+h2oServer <- h2o.init()
