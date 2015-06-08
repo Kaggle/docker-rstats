8bdf1f95d461fe290453fd83d0901faf6d899d8b
David Chudzicki <dchudz@gmail.com>
2015-06-08 13:47:22 -0400

    Update package_installs.R

--- package_installs.R
+++ package_installs.R
@@ -3,6 +3,10 @@ install_github("hadley/readr")
 install_github('dmlc/xgboost',subdir='R-package')
 install_github("jkrijthe/Rtsne")
 
+source("http://bioconductor.org/biocLite.R")
+biocLite("EBImage", ask=FALSE)
+
 # try loading the h2o package
 library(h2o)
 h2oServer <- h2o.init()
+
