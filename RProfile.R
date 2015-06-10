b9c772b61ebfeaaf21e921ec47e6b53f9f2a42df
Ben Hamner <ben@benhamner.com>
2015-06-10 11:56:01 -0700

    supressing package startup messages take 2

--- RProfile.R
+++ RProfile.R
@@ -3,8 +3,10 @@ options(repos = list(CRAN = "http://cran.rstudio.com/"))
 options(device = function() png(width = 900))
 
 # Suppressing package startup messages in package loads
+env <- as.environment('package:base') 
+unlockBinding('library', env) 
 library.warn <- library
-library <- function(
+utils::assignInNamespace('library', function(
   package, help, pos = 2, lib.loc = NULL, character.only = FALSE,
   logical.return = FALSE, warn.conflicts = TRUE, quietly = FALSE,
   verbose = getOption("verbose")) {
@@ -15,4 +17,5 @@ library <- function(
   suppressPackageStartupMessages(library.warn(
     package, help, pos, lib.loc, character.only = TRUE,
     logical.return, warn.conflicts, quietly, verbose))
-}
+}, ns="base")
+lockBinding('library', env) 
