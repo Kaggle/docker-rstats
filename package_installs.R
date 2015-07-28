d44ed5ccdfbb400bd50a8ee4315f7f0773ccb299
Ben Hamner <ben@benhamner.com>
2015-07-28 14:27:17 -0700

    removed unnecessary h20 commands

--- package_installs.R
+++ package_installs.R
@@ -5,8 +5,3 @@ install_github("jkrijthe/Rtsne")
 
 source("http://bioconductor.org/biocLite.R")
 biocLite("EBImage", ask=FALSE)
-
-# try loading the h2o package
-library(h2o)
-h2oServer <- h2o.init()
-
