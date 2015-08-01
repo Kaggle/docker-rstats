dc5cd4fc53348400cbbc21e17ca226d591d00f36
David Chudzicki <dchudz@gmail.com>
2015-08-01 17:10:29 -0400

    remove suppression of package start messages

--- RProfile.R
+++ RProfile.R
@@ -5,19 +5,20 @@ options(device = function() png(width = 900))
 # Suppressing package startup messages in package loads
 # WART: this appears dangerous and is likely the source of
 #       future tough-to-debug bugs
-env <- as.environment('package:base') 
-unlockBinding('library', env) 
-library.warn <- library
-utils::assignInNamespace('library', function(
-  package, help, pos = 2, lib.loc = NULL, character.only = FALSE,
-  logical.return = FALSE, warn.conflicts = TRUE, quietly = FALSE,
-  verbose = getOption("verbose")) {
-  if (!character.only) {
-    package <- as.character(substitute(package))
-  }
+#  (removing this for now as it caused issues with the gbm package)
+# env <- as.environment('package:base') 
+# unlockBinding('library', env) 
+# library.warn <- library
+# utils::assignInNamespace('library', function(
+#   package, help, pos = 2, lib.loc = NULL, character.only = FALSE,
+#   logical.return = FALSE, warn.conflicts = TRUE, quietly = FALSE,
+#   verbose = getOption("verbose")) {
+#   if (!character.only) {
+#     package <- as.character(substitute(package))
+#   }
 
-  suppressPackageStartupMessages(library.warn(
-    package, help, pos, lib.loc, character.only = TRUE,
-    logical.return, warn.conflicts, quietly, verbose))
-}, ns="base")
-lockBinding('library', env) 
+#   suppressPackageStartupMessages(library.warn(
+#     package, help, pos, lib.loc, character.only = TRUE,
+#     logical.return, warn.conflicts, quietly, verbose))
+# }, ns="base")
+# lockBinding('library', env) 
