d544bb284997a09b1050049f3c1eb9a54ec3c048
Jamie Hall <jamie1212@gmail.com>
2016-02-16 14:35:17 +1100

    Revert bioconductor packages

--- package_installs.R
+++ package_installs.R
@@ -3,6 +3,7 @@ install_github("hadley/readr")
 install_github("jkrijthe/Rtsne")
 install_github("slowkow/ggrepel")
 
-source("http://bioconductor.org/biocLite.R")
-biocLite("EBImage", ask=FALSE)
-install_url("http://cran.r-project.org/src/contrib/Archive/biOps/biOps_0.2.2.tar.gz")
+# Temporarily reverting Bioconductor packages until rcran is rebuilt
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("EBImage", ask=FALSE)
+#install_url("http://cran.r-project.org/src/contrib/Archive/biOps/biOps_0.2.2.tar.gz")
