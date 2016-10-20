7797ecd7f3d453c49b08b47cc064cfe94c4ccb07
Jamie Hall <jamie1212@gmail.com>
2016-10-20 15:18:47 +1100

    Update ggplot2

--- package_installs.R
+++ package_installs.R
@@ -2,6 +2,7 @@ library(devtools)
 install_github("hadley/readr")
 install_github("jkrijthe/Rtsne")
 install_github("slowkow/ggrepel")
+install_github("hadley/ggplot2")    # ggthemes is built against the latest ggplot2
 install_github("jrnold/ggthemes")
 install_github("BillPetti/baseballr")
 install_github("dahtah/imager")
