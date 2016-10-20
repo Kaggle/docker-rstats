30bc299f01ade5bc0406937d51921de8cb18b513
Jamie Hall <jamie1212@gmail.com>
2016-10-20 14:40:36 +1100

    Add automated build script

---
+++ test_build.R
@@ -0,0 +1,36 @@
+# Loosely based on http://www.kdnuggets.com/2015/06/top-20-r-packages.html
+
+Library <- function(libname){
+  print(libname)
+  suppressPackageStartupMessages(library(libname, character.only=TRUE))
+}
+
+Library("Rcpp")
+Library("ggplot2")
+Library("stringr")
+Library("plyr")
+Library("digest")
+Library("reshape2")
+Library("colorspace")
+Library("RColorBrewer")
+Library("scales")
+Library("labeling")
+Library("proto")
+Library("munsell")
+Library("gtable")
+Library("dichromat")
+Library("mime")
+Library("RCurl")
+Library("bitops")
+Library("zoo")
+Library("knitr")
+Library("dplyr")
+Library("readr")
+Library("tidyr")
+Library("randomForest")
+Library("xgboost")
+
+testPlot <- ggplot(data.frame(x=1:10,y=runif(10))) + aes(x=x,y=y) + geom_line()
+ggsave(testPlot, filename="plot1.png")
+
+print("Ok!")
