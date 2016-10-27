b954293ff4396df6fc3d65c4a4382e552819bad9
Jamie Hall <jamie1212@gmail.com>
2016-10-27 13:23:42 +1100

    Add ggforce, ggraph, gganimate, widyr

--- package_installs.R
+++ package_installs.R
@@ -4,8 +4,12 @@ install_github("jkrijthe/Rtsne")
 install_github("slowkow/ggrepel")
 install_github("hadley/ggplot2")    # ggthemes is built against the latest ggplot2
 install_github("jrnold/ggthemes")
+install_github("thomasp85/ggforce")
+install_github("thomasp85/ggraph")
+install_github("dgrtwo/gganimate")
 install_github("BillPetti/baseballr")
 install_github("dahtah/imager")
 install_github("elbamos/largevis")
+install_github("dgrtwo/widyr")
 
 install.packages("genderdata", repos = "http://packages.ropensci.org")
