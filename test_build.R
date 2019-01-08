f65167bd77e2a27b52342b26058283bf36dd5b56
Vincent Roseberry <rosbo@google.com>
2019-01-08 15:56:58 +0000

    add tests for the updated packages

--- test_build.R
+++ test_build.R
@@ -40,6 +40,7 @@ Library("fftw")
 Library("seewave")
 Library("mxnet")
 Library("lightgbm")
+Library("fslr")
 
 testPlot1 <- ggplot(data.frame(x=1:10,y=runif(10))) + aes(x=x,y=y) + geom_line()
 ggsave(testPlot1, filename="plot1.png")
