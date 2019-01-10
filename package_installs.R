f70e4887e293fb6162e5bbb1e74991987753ac70
Vincent Roseberry <rosbo@google.com>
2019-01-10 09:08:18 -0800

    Remove version pinning for LSHR
    My PR to fix an upstream bug has been merged:
    https://github.com/dselivanov/LSHR/pull/14

--- package_installs.R
+++ package_installs.R
@@ -18,9 +18,7 @@ install_github("rstudio/leaflet")
 install_url('https://github.com/catboost/catboost/releases/download/v0.12.1.1/catboost-R-Linux-0.12.1.1.tgz', args = c("--no-multiarch"))
 install_github("sassalley/hexmapr")
 install_github("hadley/multidplyr")
-# master is broken, I opened a PR on the package to fix it. Remove the commit hash below once merged.
-# https://github.com/dselivanov/LSHR/pull/14
-install_github("dselivanov/LSHR@f1d5c954393f84a0f9df5f6d96da8432b33d8594")
+install_github("dselivanov/LSHR")
 
 # install latest sparklyr and Spark (for local mode)
 install_github("rstudio/sparklyr")
