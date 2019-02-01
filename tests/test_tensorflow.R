0dd5fd1e1c059c1cecb601b99550dc2c5b2af9da
Vincent Roseberry <rosbo@google.com>
2019-02-01 22:55:11 +0000

    Add tensorflow GPU tests

--- tests/test_tensorflow.R
+++ tests/test_tensorflow.R
@@ -1,11 +1,14 @@
 context("tensorflow")
 
-# TODO(rosbo): Add tensorflow tests
-
 test_that("gpu imports", {
   check_gpu()
 
-  # TODO(rosbo): Add tensorflow GPU tests
+  library(tensorflow)
+  sess <- tf$Session()
+  with(tf$device("/gpu:0"), {
+      const <- tf$constant(42)
+      expect_equal(42, sess$run(const))
+  })
 
   expect_true(TRUE)
 })
