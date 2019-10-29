b94185df7d35ab7cea82ebf9eeaf10e1dedec25e
William Cukierski <wcukierski@google.com>
2019-10-29 17:33:15 +0000

    Fixing broken topicmodels test

--- tests/test_topicmodels.R
+++ tests/test_topicmodels.R
@@ -1,22 +1,9 @@
 context("topicmodels")
 
-# https://eight2late.wordpress.com/2015/09/29/a-gentle-introduction-to-topic-modeling-using-r/
-test_that("basic example", {
+test_that("basic topic model example", {
 	expect_error({
 		library(topicmodels)
-
-		#Set parameters for Gibbs sampling
-		burnin <- 4000
-		iter <- 2000
-		thin <- 500
-		seed <-list(2003,5,63,100001,765)
-		nstart <- 5
-		best <- TRUE
-
-		#Number of topics
-		k <- 5
-
-		#Run LDA using Gibbs sampling
-		ldaOut <-LDA(dtm,k, method="Gibbs", control=list(nstart=nstart, seed = seed, best=best, burnin = burnin, iter = iter, thin=thin))
+        	data("AssociatedPress")
+		ap_lda <- LDA(AssociatedPress, k = 2, control = list(seed = 1234))
 	}, NA) # expect no error to be thrown
 })
