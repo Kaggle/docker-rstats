f4fa9086229d67e68b2e28ad54838c4115610197
Vincent Roseberry <rosbo@google.com>
2019-11-19 23:01:22 +0000

    add bayescl tests back

--- tests/test_bayescl.R
+++ tests/test_bayescl.R
@@ -9,8 +9,6 @@ test_that("mlr", {
     X <- matrix(rnorm(100 * 10), nrow = 100)
     y <- to_categorical(matrix(sample(0:2, 100, TRUE), ncol = 1), 3)
 
-    #TODO: b/143684455 - This test is causing a segault
-    expect_equal(TRUE,TRUE)
-    #out <- mlr(y, X, samp=1000, burn=100, device=0);
-    #expect_equal(100, length(out$n))
+    out <- mlr(y, X, samp=1000, burn=100, device=0)
+    expect_equal(100, length(out$n))
 })
