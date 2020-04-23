e26e31c0e75d5415a1dceca03964bbc5a525704f
Philippe Modard <philmod@google.com>
2020-04-23 10:06:08 -0400

    Remove extra reticulate environment (#113)

--- tests_py36/test_py36.R
+++
@@ -1,8 +0,0 @@
-context("py36")
-
-test_that("python 3.6 conda env is installed", {
-    Sys.setenv(RETICULATE_PYTHON = "/opt/conda/envs/py36/bin/", required = T)
-    library("reticulate")
-    py_version <- reticulate::py_config()$python
-    expect_equal(py_version, "/opt/conda/envs/py36/bin/python")
-})
