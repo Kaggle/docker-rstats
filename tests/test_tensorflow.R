5db7d9327ac73a3ab4407b86bc9b23af05b0b21d
Philmod <philmod@google.com>
2020-06-22 13:14:34 -0400

    Upgrade CUDA, use the same as for docker-python

--- tests/test_tensorflow.R
+++ tests/test_tensorflow.R
@@ -6,9 +6,10 @@ test_that("gpu imports", {
   #TODO(b/143642025): update to TF2.0 compatible and fix potential CUDA issues.
  
   library(tensorflow)
+  sess <- tf$Session()
   with(tf$device("/gpu:0"), {
       const <- tf$constant(42)
-      expect_equal(42, as.integer(const))
+      expect_equal(42, sess$run(const))
   })
 
   expect_true(TRUE)
