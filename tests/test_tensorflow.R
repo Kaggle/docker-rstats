a5699bf25e5c892ade43c4d81be3f5067987fedc
Philmod <philmod@google.com>
2020-06-23 10:37:24 -0400

    Remove todo

--- tests/test_tensorflow.R
+++ tests/test_tensorflow.R
@@ -2,8 +2,6 @@ context("tensorflow")
 
 test_that("gpu imports", {
   check_gpu()
-
-  #TODO(b/143642025): update to TF2.0 compatible and fix potential CUDA issues.
  
   library(tensorflow)
   with(tf$device("/gpu:0"), {
