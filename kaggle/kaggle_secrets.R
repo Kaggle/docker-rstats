c391e7d539c1b79c11bf746a79d0a09b062b743e
Victor Mota <vimota@gmail.com>
2020-08-07 01:10:38 +0000

    Move JWE for user secrets to Kaggle Authorization header instead of request body.

--- kaggle/kaggle_secrets.R
+++ kaggle/kaggle_secrets.R
@@ -13,8 +13,17 @@ get_user_secret <- function(label) {
     if (KAGGLE_USER_SECRETS_TOKEN == '') {
       stop("Expected KAGGLE_USER_SECRETS_TOKEN environment variable to be present.", call. = FALSE)
     }
-    request_body <- list(JWE = KAGGLE_USER_SECRETS_TOKEN, Label = label)
-    response <- POST(paste0(KAGGLE_BASE_URL, GET_USER_SECRET_BY_LABEL_ENDPONT), body = request_body, encode = "json")
+    request_body <- list(Label = label)
+    auth_header <- paste0("Bearer ", KAGGLE_USER_SECRETS_TOKEN)
+    headers <- add_headers(c("X-Kaggle-Authorization" = auth_header))
+    response <- POST(
+      paste0(KAGGLE_BASE_URL,GET_USER_SECRET_BY_LABEL_ENDPONT),
+      headers,
+      # Reset the cookies on each request, since the server expects none.
+      handle = handle(''),
+      body = request_body,
+      encode = "json"
+    )
     if (http_error(response) || !identical(content(response)$wasSuccessful, TRUE)) {
       err <- paste("Unable to get user secret. Please ensure you have internet enabled. Error: ",
                         paste(content(response, "text", encoding = 'utf-8')))
