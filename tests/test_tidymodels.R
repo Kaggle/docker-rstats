3a32c79a6e7196b863d4c083e22429707daf867e
Philmod <philmod@google.com>
2020-11-26 11:09:39 -0500

    Add a test for one of the tidymodels' package: broom

--- tests/test_tidymodels.R
+++ tests/test_tidymodels.R
@@ -3,3 +3,9 @@ context("tidymodels")
 test_that("tidymodels exists", {
 	library(tidymodels)
 })
+
+test_that("broom", {
+	library(broom)
+	fit <- lm(Volume ~ Girth + Height, trees)
+	expect_equal(ncol(tidy(fit)), 5)
+})
