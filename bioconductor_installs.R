49776bb3117fc897d6f5f6bb86f1dccf8bf3c55a
Philmod <philmod@google.com>
2020-12-16 17:01:33 -0500

    Install devtools if missing

--- bioconductor_installs.R
+++ bioconductor_installs.R
@@ -8,5 +8,7 @@ BiocManager::install("EBImage", ask=FALSE)
 BiocManager::install("rhdf5", ask=FALSE)
 BiocManager::install("limma", ask=FALSE)
 
+if("devtools" %in% rownames(installed.packages()) == FALSE) {install.packages("devtools")}
 library(devtools)
+
 install_url("http://cran.r-project.org/src/contrib/Archive/biOps/biOps_0.2.2.tar.gz", quiet=TRUE)
