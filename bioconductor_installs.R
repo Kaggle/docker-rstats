e9ab59de1e1360750786400ae516171420568bf0
Philippe Modard <philmod@google.com>
2020-12-21 12:47:58 -0500

    Fix devtools missing when installing bioconductor (#142)
    http://b/175794013

--- bioconductor_installs.R
+++ bioconductor_installs.R
@@ -1,3 +1,9 @@
+options(repos = c("CRAN" = "http://cran.us.r-project.org"))
+options(Ncpus = parallel::detectCores())
+
+install.packages("devtools")
+library(devtools)
+
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
@@ -8,5 +14,4 @@ BiocManager::install("EBImage", ask=FALSE)
 BiocManager::install("rhdf5", ask=FALSE)
 BiocManager::install("limma", ask=FALSE)
 
-library(devtools)
 install_url("http://cran.r-project.org/src/contrib/Archive/biOps/biOps_0.2.2.tar.gz", quiet=TRUE)
