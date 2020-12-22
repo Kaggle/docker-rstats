a18d89138580dd2c791c977ffb91c22da5d3489a
Philmod <philmod@google.com>
2020-12-22 11:29:13 -0500

    Remove biOps package which wasn't used in the past 60 days

--- bioconductor_installs.R
+++ bioconductor_installs.R
@@ -8,10 +8,7 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
 BiocManager::install(ask=FALSE)
-
 BiocManager::install("BiocGenerics", ask=FALSE)
 BiocManager::install("EBImage", ask=FALSE)
 BiocManager::install("rhdf5", ask=FALSE)
 BiocManager::install("limma", ask=FALSE)
-
-install_url("http://cran.r-project.org/src/contrib/Archive/biOps/biOps_0.2.2.tar.gz", quiet=TRUE)
