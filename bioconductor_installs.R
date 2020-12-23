2af7dc93574bdfe0da7386b9e73bfe6b627d998e
Philmod <philmod@google.com>
2020-12-23 09:16:12 -0500

    Do not update Bioc packages

--- bioconductor_installs.R
+++ bioconductor_installs.R
@@ -1,13 +1,15 @@
 options(repos = c("CRAN" = "http://cran.us.r-project.org"))
 options(Ncpus = parallel::detectCores())
 
-install.packages("devtools")
+if("devtools" %in% rownames(installed.packages()) == FALSE)
+	install.packages("devtools")
 library(devtools)
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
-BiocManager::install("BiocGenerics", ask=FALSE)
+BiocManager::install(update=FALSE, ask=FALSE)
+BiocManager::install("BiocGenerics", update=FALSE, ask=FALSE)
 BiocManager::install("EBImage", ask=FALSE)
 BiocManager::install("rhdf5", ask=FALSE)
 BiocManager::install("limma", ask=FALSE)
