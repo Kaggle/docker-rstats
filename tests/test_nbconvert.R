1219de1e45cfe811c29d3b93c383ead11de7a69f
Philmod <philmod@google.com>
2021-04-20 12:28:27 -0400

    Test nbconvert to html

--- tests/test_nbconvert.R
+++ tests/test_nbconvert.R
@@ -1,6 +1,6 @@
 context("nbconvert")
 
-test_that("nbconvert exists", {
+test_that("nbconvert to notebook", {
 	expect_error({
 		library(jsonlite)
 
@@ -8,5 +8,13 @@ test_that("nbconvert exists", {
 			intern = TRUE)
 		json <- fromJSON(results, simplifyVector = FALSE)
 		expect_equal(json$cells[[1]]$outputs[[1]]$text[[1]], "[1] 999\n")
+    }, NA) # expect no error to be thrown
+})
+
+test_that("nbconvert to html", {
+	expect_error({
+		results <- system("jupyter nbconvert --to html --output \"__results__.html\" --template /opt/kaggle/nbconvert-extensions.tpl --Exporter.preprocessors=[\\\"nbconvert.preprocessors.ExtractOutputPreprocessor\\\"] \"/input/tests/data/notebook.ipynb\"",
+			intern = TRUE)
+		expect_match(results, "Hello World")
     }, NA) # expect no error to be thrown
 })
