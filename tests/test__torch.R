d57804d0be337364a75656d2737cc2047ac0efcd
Philmod <philmod@google.com>
2022-04-06 22:44:42 -0400

    run torch tests first to avoid CUDA error

---
+++ tests/test__torch.R
@@ -0,0 +1,20 @@
+# This test is run first, otherwise the GPU test fails with `CUDA error: out of memory`.
+
+context("torch")
+
+test_that("cpu imports", {
+  library(torch)
+  x <- array(runif(8), dim = c(2, 2, 2))
+  y <- torch_tensor(x, dtype = torch_float64())
+  expect_identical(x, as_array(y))
+})
+
+test_that("gpu imports", {
+  check_gpu()
+ 
+  library(torch)
+  x <- array(runif(8), dim = c(2, 2, 2))
+  y <- torch_tensor(x, dtype = torch_float64(), device = "cuda")
+  expect_identical(x, as_array(y$cpu()))
+})
+
