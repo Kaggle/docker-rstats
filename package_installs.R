4c3d38782cc3196622eb411b37ca6634e4c25aa8
Görkem <gocivici@gmail.com>
2022-09-06 16:17:46 +0300

    Update package_installs.R
    added leaflet.extras package

--- package_installs.R
+++ package_installs.R
@@ -14,6 +14,7 @@ lgbdl::lgb.dl(compiler = "gcc", commit = "tags/v2.3.1")
 install_github("dgrtwo/widyr")
 install_github("ellisp/forecastxgb-r-package/pkg")
 install_github("rstudio/leaflet")
+install_github("bhaskarvk/leaflet.extras")
 # install_github fails for catboost.
 # Following direct installation instructions instead: https://tech.yandex.com/catboost/doc/dg/installation/r-installation-binary-installation-docpage/
 install_url('https://github.com/catboost/catboost/releases/download/v0.23.2/catboost-R-Linux-0.23.2.tgz', INSTALL_opts = c("--no-multiarch"))
