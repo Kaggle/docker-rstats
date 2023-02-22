2eb9cef3c416a55e9e8b50ac2db39a6176ccdf82
Philippe Modard <philmod@google.com>
2023-02-22 01:19:57 +0000

    remove tfhub test which needs internet

--- tests/test_tfhub.R
+++
@@ -1,8 +0,0 @@
-context("tfhub")
-
-test_that("tfhub", {
-    library(tfhub)
-    module <- hub_load("https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/2")
-    input <- tf$random$uniform(shape = shape(1,224,224,3), minval = 0, maxval = 1)
-    output <- module(input)
-})
